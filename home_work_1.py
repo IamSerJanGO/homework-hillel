9436c136c46b36ff30c4f6fc03885e39f918eea3
Serjan <1000remons1000@gmail.com>
2023-04-14 16:24:13 +0300

    First commit

---
+++ home_work_1.py
@@ -0,0 +1,10 @@
+# первое задание:
+print('Hello world')
+# второе задание:
+print('*****')
+# третье задание:
+x = '# # # # #'
+print(x, x, x, x, x, sep='\n')
+# or
+x = '#####'
+print(x, x, x, x, x, sep='\n')
---
+++ home_work_2.py
@@ -0,0 +1,1 @@
+print('Hallo world, i was able to do it')
