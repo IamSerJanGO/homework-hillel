c48c396d5de106e0991668507876ed5005abc070
Serjan <1000remons1000@gmail.com>
2023-04-25 23:03:01 +0300

    Треугольники ....

--- home_work_5.2.py
+++ home_work_5.2.py
@@ -1,11 +1,14 @@
 # p.s. Я вообще в шоке как этот топорный код заработал !!!
 
+
 x = int(input('Enter number:'))
 z = x * '*'
+print('1)')
 for num1 in range(1, x + 1):
     print(z)
     z = (x - num1) * '*'
-print('Finish 1')
+
+print('2)')
 
 z = '*'
 n = 1
@@ -13,17 +16,17 @@ for num2 in range(1, x + 1):
     print(z)
     n += 1
     z = n * '*'
-print('Finish 2')
 
+print('3)')
 
 z = x * '*'
 c = ' '
-
-print(z)
 for num3 in range(1, x + 1):
-    z = (c * num3) + ((x - num3) * '*')
     print(z)
-print('Finish 3')
+    z = (c * num3) + ((x - num3) * '*')
+
+
+print('4)')
 
 z = '*'
 c = ' '
@@ -31,6 +34,3 @@ for num4 in range(1, x + 1):
     c = (x - num4) * ' '
     z = num4 * '*'
     print(c + z)
-print('Finish 4')
-
-
