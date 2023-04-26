c73ef0fafdfff209217e29a65510cf8071219e12
Serjan <1000remons1000@gmail.com>
2023-04-26 10:20:43 +0300

    Деление на 3(исправил ошибку)

--- home_work_5.1.py
+++ home_work_5.1.py
@@ -1,3 +1,4 @@
+'''
 summ = 0
 x = int(input('Введите ваше число: '))
 for i in range(0, x + 1):
@@ -5,3 +6,14 @@ for i in range(0, x + 1):
         summ += i
         print('Число', i, 'делится на 3')
 print('Сумма всех чисел делящихся на 3 без остатка:', summ)
+'''
+
+summ = 0
+x = int(input('Введите ваше число: '))
+y = 0
+while y <= x:
+    if y % 3 == 0:
+        summ += y
+        print(y)
+    y += 1
+print('Сумма чисел делящихся рна 3:', summ)
