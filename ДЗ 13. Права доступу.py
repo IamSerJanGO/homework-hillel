aa0c3ffe475a1de7bb6e711b89eec8e05d2c748c
Serjan <1000remons1000@gmail.com>
2023-05-12 18:47:53 +0300

    ДЗ 13: Права доступу

--- ДЗ 13. Права доступу.py
+++ ДЗ 13. Права доступу.py
@@ -1,22 +1,20 @@
 # Это какой-то п....ц
 
-list_oper = {'read': 'R',
-             'write': 'W',
-             'execute': 'X'
-             }
+dikt_operation = {'read': 'R', 'write': 'W', 'execute': 'X'}
 
-quantity_files = int(input('Введите количество ваших файлов: '))
-print('Возможные операции с файлами:', 'W - write', 'R - read', 'X - execute', sep='\n')
+num_files = int(input('Введите количество ваших файлов: '))
+for key, operation in dikt_operation.items():
+    print(key, operation)
 files = {}
-for i in range(quantity_files):
-    files_name, *type_operation = input('Название файла, через пробел возможные операции с файлом: ').split(' ')
+for i in range(num_files):
+    files_name, *type_operation = input('Название файла, через пробел возможные операции с файлом: ').split('')
     files[files_name] = set(type_operation)
 # print(files, sep='\n') - оставил для себя - удобно проверять код
 
-quantity_operation = int(input('Количество запросов к файлам: '))
+num_operations = int(input('Количество запросов к файлам: '))
 finish_list = []
-for q in range(quantity_operation):
-    if list_oper[input('Желаемая операция с файлом: ')] in files[input('Название файла: ')]:
+for q in range(num_operations):
+    if dikt_oper[input('Желаемая операция с файлом: ')] in files[input('Название файла: ')]:
         finish_list.append('OK')
     else:
         finish_list.append('Access denied')
