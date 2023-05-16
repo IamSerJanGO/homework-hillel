b9b33f9beb46a2bd508e0e5da5b5d4d80dab9e5a
Serjan <1000remons1000@gmail.com>
2023-05-16 13:54:56 +0300

    ДЗ 14. Структури даних

---
+++ ДЗ 14. Структури даних.py
@@ -0,0 +1,21 @@
+# Первое условие !
+july = {'Nik', 'Ivan', 'Sema', 'Vova', 'Sasha', 'Tolik', 'Alex', 'Kolya', 'Andrey'}
+june = {'Max', 'Artem', 'Jenya', 'Nikita', 'Kirill', 'Nik', 'Ivan', 'Sema', 'Vova'}
+print(f'Имена должников за июль и за июнь: {",".join(july.intersection(june))}')
+print(f'Должники за Июль у которых нет долга за Июнь: {",".join(july.difference(june))}')
+
+# Второе условие
+test_list = ['ListTest', 'SerJan', 'GoogleDisk']
+new_list = []
+
+for object_list in test_list:
+    str_object = ''
+    for i in range(len(object_list)):
+        if object_list[i].isupper():
+            if i != 0:
+                str_object += '_'
+            str_object += object_list[i].lower()
+        else:
+            str_object += object_list[i]
+    new_list.append(str_object)
+print(new_list)
